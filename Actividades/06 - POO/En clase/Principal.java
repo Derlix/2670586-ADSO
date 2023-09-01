afeff204f93aba7a042e596f42dfdafa35ebc213
Christian Arias <christianariasloaiza69@gmail.com>
2023-09-01 10:56:01 -0500

    Tarea persona

--- Actividades/06 - POO/En clase/Principal.java
+++ Actividades/06 - POO/En clase/Principal.java
@@ -19,7 +19,13 @@ public class Principal {
             System.out.println("| 2: Ver lista de Personas               |");
             System.out.println("| 3: Editar Persona                      |");
             System.out.println("| 4: Eliminar Persona                    |");
-            System.out.println("| 5: Salir                               |");
+            System.out.println("| 5: Persona mayor edad                  |");
+            System.out.println("| 6: Persona menor edad                  |");
+            System.out.println("| 7: Persona más alta                    |");
+            System.out.println("| 8: Persona más baja                    |");
+            System.out.println("| 9: Promedio de edades                  |");
+            System.out.println("| 10: Promedio de altura                 |");
+            System.out.println("| 11: Salir                              |");
             System.out.println("+----------------------------------------|");
             System.out.print(" -} Ingresa una opcion:");
             opcion = entradaNums.nextInt();
@@ -31,7 +37,7 @@ public class Principal {
                 System.out.println("+----------------------------------------+");
                 System.out.print(" Ingrese Cedula:");
                 int cedula = entradaNums.nextInt();
-                entradaTexto.nextLine(); // Limpiar el búfer de entrada
+                entradaTexto.nextLine();
                 System.out.print(" Ingrese Nombres:");
                 String nombres = entradaTexto.nextLine();
                 System.out.print(" Ingrese Apellidos:");
@@ -71,7 +77,7 @@ public class Principal {
                 int cedula = entradaNums.nextInt();
 
                 int indiceEncontrada = -1;
-                entradaTexto.nextLine(); // Limpiar el búfer de entrada
+                entradaTexto.nextLine();
                 for (int i = 0; i < listadepepersona.length; i++) {
                     if (listadepepersona[i] != null && listadepepersona[i].getCedula() == cedula) {
                         indiceEncontrada = i;
@@ -109,7 +115,7 @@ public class Principal {
                 int cedula = entradaNums.nextInt();
 
                 int indiceEncontrada = -1;
-                entradaTexto.nextLine(); // Limpiar el búfer de entrada
+                entradaTexto.nextLine();
                 for (int i = 0; i < listadepepersona.length; i++) {
                     if (listadepepersona[i] != null && listadepepersona[i].getCedula() == cedula) {
                         indiceEncontrada = i;
@@ -129,6 +135,123 @@ public class Principal {
             }
 
             if (opcion == 5) {
+
+                int mayorEdad = -1;
+                Persona personaMayorEdad = null;
+
+                for (int i = 0; i < listadepepersona.length; i++) {
+                    if (listadepepersona[i] != null && listadepepersona[i].getEdad() > mayorEdad) {
+                        mayorEdad = listadepepersona[i].getEdad();
+                        personaMayorEdad = listadepepersona[i];
+                    }
+                }
+
+                if (personaMayorEdad != null) {
+                    System.out.println("Persona con mayor edad:");
+                    System.out.println(personaMayorEdad.getInfo());
+                } else {
+                    System.out.println("No hay personas registradas.");
+                }
+            }
+            if (opcion == 6) {
+
+                int menorEdad = Integer.MAX_VALUE;
+                Persona personaMenorEdad = null;
+
+                for (int i = 0; i < listadepepersona.length; i++) {
+                    if (listadepepersona[i] != null && listadepepersona[i].getEdad() < menorEdad) {
+                        menorEdad = listadepepersona[i].getEdad();
+                        personaMenorEdad = listadepepersona[i];
+                    }
+                }
+
+                if (personaMenorEdad != null) {
+                    System.out.println("Persona con menor edad:");
+                    System.out.println(personaMenorEdad.getInfo());
+                } else {
+                    System.out.println("No hay personas registradas.");
+                }
+            }
+            if (opcion == 7) {
+                int alturaMaxima = 0;
+                int indicePersonaMasAlta = -1;
+
+                for (int i = 0; i < listadepepersona.length; i++) {
+                    if (listadepepersona[i] != null && listadepepersona[i].getAltura() > alturaMaxima) {
+                        alturaMaxima = listadepepersona[i].getAltura();
+                        indicePersonaMasAlta = i;
+                    }
+                }
+
+                if (indicePersonaMasAlta != -1) {
+                    System.out.println(
+                            "La persona más alta es: " + listadepepersona[indicePersonaMasAlta].getNombres() + " "
+                                    + listadepepersona[indicePersonaMasAlta].getApellidos());
+                    System.out.println("Altura: " + listadepepersona[indicePersonaMasAlta].getAltura());
+                } else {
+                    System.out.println("No hay personas registradas para determinar la más alta.");
+                }
+
+            }
+            if (opcion == 8) {
+
+                int alturaMinima = Integer.MAX_VALUE;
+                int indicePersonaMasBaja = -1;
+
+                for (int i = 0; i < listadepepersona.length; i++) {
+                    if (listadepepersona[i] != null && listadepepersona[i].getAltura() < alturaMinima) {
+                        alturaMinima = listadepepersona[i].getAltura();
+                        indicePersonaMasBaja = i;
+                    }
+                }
+
+                if (indicePersonaMasBaja != -1) {
+                    System.out.println(
+                            "La persona más baja es: " + listadepepersona[indicePersonaMasBaja].getNombres() + " "
+                                    + listadepepersona[indicePersonaMasBaja].getApellidos());
+                    System.out.println("Altura: " + listadepepersona[indicePersonaMasBaja].getAltura());
+                } else {
+                    System.out.println("No hay personas registradas para determinar la más baja.");
+                }
+            }
+            if (opcion == 9) {
+                int sumaEdades = 0;
+                int totalPersonas = 0;
+
+                for (int i = 0; i < listadepepersona.length; i++) {
+                    if (listadepepersona[i] != null) {
+                        sumaEdades += listadepepersona[i].getEdad();
+                        totalPersonas++;
+                    }
+                }
+
+                if (totalPersonas > 0) {
+                    double promedioEdades = (double) sumaEdades / totalPersonas;
+                    System.out.println("Promedio de edades: " + promedioEdades);
+                } else {
+                    System.out.println("No hay personas registradas para calcular el promedio de edades.");
+                }
+
+            }
+            if (opcion == 10) {
+                int sumaAlturas = 0;
+                int totalPersonas = 0;
+
+                for (int i = 0; i < listadepepersona.length; i++) {
+                    if (listadepepersona[i] != null) {
+                        sumaAlturas += listadepepersona[i].getAltura();
+                        totalPersonas++;
+                    }
+                }
+
+                if (totalPersonas > 0) {
+                    double promedioAlturas = (double) sumaAlturas / totalPersonas;
+                    System.out.println("Promedio de altura: " + promedioAlturas);
+                } else {
+                    System.out.println("No hay personas registradas para calcular el promedio de altura.");
+                }
+            }
+            if (opcion == 11) {
                 System.out.println("\n");
                 System.out.println(" SALIENDO....");
                 System.out.println("\n");
