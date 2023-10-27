a8692e885881858440c6eb6dd0d1d1693bd025f9
M4TTar <jkleymattar@gmail.com>
2023-10-27 14:55:11 -0500

    POO

--- Actividades/06 - POO - Actividad 12/Principal.java
+++ Actividades/06 - POO - Actividad 12/Principal.java
@@ -9,6 +9,10 @@ public class Principal {
         int cursoCount = 0;
         int opcion = 0;
 
+        cursos[0]=new Cursos("1089", "Ingles", "Tobe", 48, 3);
+        cursos[1]=new Cursos("188", "Matematicas", "Funciones", 102,4);
+
+
         while (opcion != 7) {
             System.out.println("+--------------------------------------------+");
             System.out.println("|                MENU DE CURSOS              |");
@@ -28,12 +32,26 @@ public class Principal {
             if (opcion == 1) {
 
                 if (cursoCount < 100) {
-                    cursos[cursoCount] = new Cursos("", "", "", 0, null); // Crear una instancia de Cursos
-                    cursos[cursoCount] = cursos[cursoCount].crearCurso(); // Llamar a crearCurso en la instancia
+                    System.out.print("Agrega el codigo del curso: ");
+                    String codigo = leer.nextLine();
+
+                    System.out.print("Pon un nombre para este curso: ");
+                    String nombre = leer.nextLine();
+                    System.out.print("Escribe el area de conocimiento: ");
+                    String areadeconocimiento = leer.nextLine();
+                    System.out.print("Ingresa las horas del curso: ");
+                    int DuracionEnHoras = num.nextInt();
+                    
+                    System.out.print("Ingresa las cantidad de temas: ");
+                    int cantidadTemas = num.nextInt();
+                    
+        
+                    cursos[cursoCount] = new Cursos(codigo, nombre, areadeconocimiento, DuracionEnHoras, cantidadTemas);
+                    // Crear una instancia de Cursos
                     cursoCount++;
                 } else {
                     System.out.println("LA MEMORIA ESTA LLENA NO PUEDES");
-                    System.out.println("     AGREGAR MAS CURSOS");
+                    System.out.println("     AGREGAR MAS CURSOS        ");
                 }
             }
             if (opcion == 2) {
@@ -52,7 +70,7 @@ public class Principal {
 
                 for (int i = 0; i < cursoCount; i++) {
                     if (cursos[i] != null && cursos[i].getCodigo().equals(code)) {
-                        cursos[i].imprimir_detalle();
+                        cursos[i].imprimir_detalles();
                         cursoEncontrado = true;
                         break; // Terminamos el bucle porque ya encontramos el curso
                     }
@@ -70,26 +88,59 @@ public class Principal {
                         System.out.println(" Ficha: " + cursos[i].getCodigo());
                     }
                 }
-                int indice = num.nextInt();
-                cursos[indice].editar(indice, cursos);
-            }
-            if (opcion == 5) {
-                System.out.println(" Cuantos temas quieres agregar (No más de 14) ");
-                int cantidadTemas = num.nextInt();
-                System.out.print("Código del curso al que deseas agregar un tema: ");
-                String codigoCurso = leer.nextLine();
-
+                int code = num.nextInt();
+                boolean cursoEncontrado = false;
                 for (int i = 0; i < cursoCount; i++) {
-                    if (cursos[i] != null && cursos[i].getCodigo().equals(codigoCurso)) {
-                        for (int j = 0; j < cantidadTemas; j++) {
-                            System.out.print("Nombre del tema: ");
-                            String nuevoTema = leer.nextLine();
-                            cursos[i].agregarTema(nuevoTema);
-                            break; // Termina el bucle una vez que se ha encontrado y actualizado el curso.
-                        }
+                    if (cursos[i] != null && cursos[i].getCodigo().equals(code)) {
+                        
+                        System.out.println(" Nuevo Codigo ");
+                        String newcode = leer.nextLine();
+                        System.out.println(" Nuevo Nombre ");
+                        String newnombre = leer.nextLine();
+                        System.out.println("Nuevo Area de conocimiento ");
+                        String newnareadec = leer.nextLine();
+                        System.out.println("Nueva duracion ");
+                        int newnduracion = leer.nextInt();
+                        System.out.println("nuevo tema " );
 
+                        cursos[i].setCodigo(newcode);
+                        cursos[i].setNombre(newnombre);
+                        cursos[i].setAreadeconocimiento(newnareadec);
+                        cursos[i].setDuracionEnHoras(newnduracion);
+                        System.out.println("+-----------------------------+");
+                        System.out.println("| Curso editado correctamente |");
+                        System.out.println("+-----------------------------+");
+
+                        cursoEncontrado = true;
+                        break; // Terminamos el bucle porque ya encontramos el curso
                     }
                 }
+                if (cursoEncontrado!= false) {
+                    System.out.println(" El curso no ha sido encontrado ");
+                }
+                
+            }
+            if (opcion == 5) {
+                
+                System.out.print("Codigo del curso al que deseas agregar un tema: ");
+                String codigoCurso = leer.nextLine();
+                boolean cursoEncontrado = false;
+                for (int i = 0; i < cursoCount; i++) {
+                    if (cursos[i] != null && cursos[i].getCodigo().equals(codigoCurso)) {
+                        
+                        System.out.print("Nombre del tema: ");
+                        String nuevoTema = leer.nextLine();
+                        cursos[i].agregarTema(nuevoTema);
+                        break; // Termina el bucle una vez que se ha encontrado y actualizado el curso.
+                        
+                    }
+                }
+                if (cursoEncontrado!= true) {
+                    System.out.println(" El curso no ha sido encontrado ");
+                }
+
+            }
+            if (opcion == 6){
 
             }
 
@@ -98,9 +149,10 @@ public class Principal {
                 System.out.println("|   Saliendo...  | ");
                 System.out.println("+----------------+ ");
             }
-            leer.close();
-            num.close();
+            
         }
+        leer.close();
+        num.close();
     }
 
 }
