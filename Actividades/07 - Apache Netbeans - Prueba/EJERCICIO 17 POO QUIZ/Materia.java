3701f2a2f96f0331912fc4b4f516d1000c86ec31
Christian Arias <christianariasloaiza69@gmail.com>
2024-02-07 20:49:14 -0500

    NetBeans ejercicios prueba Promedio y Tabla Basica

---
+++ Actividades/07 - Apache Netbeans - Prueba/EJERCICIO 17 POO QUIZ/Materia.java
@@ -0,0 +1,36 @@
+public class Materia {
+
+    private String nombre;
+    private float creditos;
+    private float nota;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(float creditos) {
+        this.creditos = creditos;
+    }
+
+    public float getNota() {
+        return nota;
+    }
+
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+
+    public Materia(String nombre, float creditos, float nota) {
+        this.nombre = nombre;
+        this.creditos = creditos;
+        this.nota = nota;
+    }
+}
