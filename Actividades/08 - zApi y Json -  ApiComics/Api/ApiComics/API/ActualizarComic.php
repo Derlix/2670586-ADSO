595d129e235ee30a2c06a5a2e5a4ef6cacd10c0c
Christian Arias <christianariasloaiza69@gmail.com>
2024-06-09 19:24:07 -0500

    Arreglando Api

--- Actividades/08 - zApi y Json -  ApiComics/Api/ApiComics/API/ActualizarComic.php
+++ Actividades/08 - zApi y Json -  ApiComics/Api/ApiComics/API/ActualizarComic.php
@@ -6,14 +6,16 @@
         $titulo = $_POST['titulo'];
         $autor = $_POST['autor'];
         $total_paginas = $_POST['total_paginas'];
+        $fecha_lanzamiento = $_POST['fecha_lanzamiento'];
 
 
         try {
-            $consulta = $base_de_datos->prepare("UPDATE personas SET autor=:aut, total_paginas=:tot, fecha_lanzamiento=:fec WHERE titulo = :tit ");
+            $consulta = $base_de_datos->prepare("UPDATE comics SET autor=:aut, total_paginas=:tot, fecha_lanzamiento=:fec WHERE titulo = :tit ");
 
             $consulta->bindParam(':tit', $titulo);
             $consulta->bindParam(':aut', $autor);
             $consulta->bindParam(':tot', $total_paginas);
+            $consulta->bindParam(':fec', $fecha_lanzamiento);
 
             
             $proceso = $consulta->execute();
